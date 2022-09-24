53a9db936f2e679c9f2d68be11c1989262784268
psychtr0pia <alejandro.villalobos@alumnos.uv.cl>
2022-09-24 03:02:35 -0300

    funciones

--- paginaWEB/app/controllers/admin.controller.js
+++ paginaWEB/app/controllers/admin.controller.js
@@ -1,4 +1,6 @@
 //dependencias
+const { query } = require("express");
+const { admin } = require("../models");
 const db = require("../models");
 const Admin = db.admin;
 const Op = db.Sequelize.Op;
@@ -12,12 +14,12 @@ if(!req.body.rut){
 }
 //Crear un nuevo Admin
 const admin = {
-	RUT: req.body.rut,
-	PASSWORD: req.body.password,
+	RUT: req.body.RUT,
+	PASSWORD: req.body.PASSWORD,
 };
 //Guardarlo en la base de datos
 Admin.create(admin).then(data => {
-	res.send(data);
+	res.status(200).send(data);
 })
 .catch(err => {
 	res.status(500).send({
@@ -26,8 +28,58 @@ Admin.create(admin).then(data => {
 	});
 });
 };
-//Buscar a ADMIN por RUT
+//Buscar a todos los ADMINS (solo devuelve RUTs)
+exports.findAll = (req, res) => {
+	let admins = admins.findAll({
+		attributes: [
+			'RUT'
+		]
+	})
+	res.status(200).send(admins);
+};
+
 //Actualizar password ADMIN
+exports.update = (req, res) => {
+	const RUT = req.body.RUT;
+	Admin.update(req.body, {where: {RUT: RUT}})
+	.then(num => {
+        if (num == 1) {
+            res.status(200).send({
+                message: "Password de admin actualizado."
+            });
+        } else {
+            res.send({
+                message: `No se pudo actualizar la password del Admin`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Error en actualización"
+        });
+    });
+
+};
 //Eliminar ADMIN
+exports.delete = (req, res) => {
+	const RUT = req.body.RUT;
+	Admin.destroy({where: {RUT: RUT}})
+	.then(num => {
+        if (num == 1) {
+            res.status(200).send({
+                message: "Admin eliminado de la base de datos."
+            });
+        } else {
+            res.send({
+                message: `No se pudo eliminar Admin de la base de datos.`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Error en eliminacion"
+        });
+    });
+};
 //Eliminar a todos los admins
 
