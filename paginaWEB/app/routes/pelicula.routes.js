4ed28bc09a558697634d747055881d9183a7b07a
psychtr0pia <alejandro.villalobos@alumnos.uv.cl>
2022-09-25 02:55:24 -0300

    fucniones y rutas de pelicula

---
+++ paginaWEB/app/routes/pelicula.routes.js
@@ -0,0 +1,16 @@
+module.exports = app => {
+	const pelicula = require("../controllers/pelicula.controller.js");
+	var router = require("express").Router();
+	//Crear pelicula
+	router.post("/", pelicula.create);
+	//Buscar todas las peliculas
+	router.get("/", pelicula.findAll)
+    //Buscar una sola pelicula
+    router.get("/:id_pelicula", pelicula.findOne)
+	//Actualizar pelicula
+	router.put("/:id_pelicula", pelicula.update);
+	//Eliminar pelicula
+	router.delete("/:id_pelicula", pelicula.delete);
+	//agregar rutas al servidor
+	app.use('/api/admin', router);
+};
